1928ccc6f056c22772538bb2689f0a4d79a13a59
PackYokohama <59277976+PackYokohama@users.noreply.github.com>
2020-01-07 03:54:33 +0700

    Update lab4_2.cpp

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
-
-//Write function findDistance() here
-
+double findDisance(double U,double A,double T){
+ return U*T+0.5*A*pow(T,2);
+}
 int main(){
-
-  //Calling findDistance() using test cases
-
+double U;
+double A;
+double T;
+double S;
+cout << "Enter U\n";
+cin >> U;
+cout << "Enter A\n";
+cin >> A;
+cout << "Enter T\n";
+cin >> T;
+S=findDisance(U,A,T);
+cout << S;
   return 0;
 }
